dfa79af0a1a85f26acb508d1b25f8d6389fdd52e
DangZz07 <vuongvandang14072004@gmail.com>
2024-10-28 21:41:39 +0700

    Final

---
+++ AimsProject/src/Cart.java
@@ -0,0 +1,46 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    public int qtyOrdered=0;
+    
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered > 0){
+            for (int i = 0; i < qtyOrdered; i++) {
+                if (itemOrdered[i].equals(disc)) {
+                    itemOrdered[i] = itemOrdered[qtyOrdered - 1];
+                    itemOrdered[qtyOrdered - 1] = null;
+                    qtyOrdered--;
+                    System.out.println("The disc has been removed.");
+                }
+            }
+            
+        }
+        else {
+        	System.out.println("Your cart dont have any disc.");
+        }
+       
+
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered == MAX_NUMBERS_ORDERED){
+            System.out.println("The cart is almost full.");
+        }
+        else {
+            itemOrdered[qtyOrdered++] = disc;
+            System.out.println("The disc has been added.");
+        	
+        }
+    }
+
+
+    public float totalCost(){
+        float total = 0;
+        int i = 0;
+        while(i < qtyOrdered) {
+        	total += itemOrdered[i].getCost();
+        	i++;
+        }
+        return total;
+    }
+}
